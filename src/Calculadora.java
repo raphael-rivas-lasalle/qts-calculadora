37105f9d65e7ed5a867870306baa2ad0cf2ce07a
raphael-rivas-lasalle <raphael.rivas@soulasalle.com.br>
2025-03-14 21:35:09 -0300

    Add files via upload

---
+++ src/Calculadora.java
@@ -0,0 +1,42 @@
+public final class Calculadora {
+
+    /**
+     * Soma dois números.
+     * @param valor1 o primeiro número
+     * @param valor2 o segundo número
+     * @return a soma de valor1 e valor2
+     */
+    public double somar(final double valor1, final double valor2) {
+        return valor1 + valor2;
+    }
+
+    /**
+     * Subtrai dois números.
+     * @param valor1 o primeiro número
+     * @param valor2 o segundo número
+     * @return a soma de valor1 e valor2
+     */
+    public double subtrair(final double valor1, final double valor2) {
+        return valor1 - valor2;
+    }
+
+    /**
+     * Multiplica dois números.
+     * @param valor1 o primeiro número
+     * @param valor2 o segundo número
+     * @return a multiplicação de valor1 e valor2
+     */
+    public double multiplicar(final double valor1, final double valor2) {
+        return valor1 * valor2;
+    }
+
+    /**
+     * Divide dois números.
+     * @param valor1 o primeiro número
+     * @param valor2 o segundo número
+     * @return a divisão de valor1 e valor2
+     */
+    public double dividir(final double valor1, final double valor2) {
+        return valor1 / valor2;
+    }
+}
